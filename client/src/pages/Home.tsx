49d495e7b3ccafd17f0780a213c99e003d78dec5
Cindy Sy <sy.cindyrose@gmail.com>
2023-11-29 01:25:17 -0700

    Make cards responsive

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,7 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import PricingCard from '../components/landing-page/PricingCard';
-import Footer from '../components/Footer';
+import { Grid } from '@mui/material';
 
 function Home() {
   const primaryColor = "#344055";
@@ -34,9 +30,17 @@ function Home() {
           <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"row"} marginTop={"30px"}>
             
             {/* Tier Cards */}
-            <PricingCard tier="free" price="free"/>
-            <PricingCard tier="basic" price="free"/>
-            <PricingCard tier="premium" price="5.99 per month"/>
+            <Grid container display={"flex"} justifyContent="center" alignItems={"stretch"}>
+              <Grid item style={{ display: "flex" }}>
+                <PricingCard tier="free" price="free"/>
+              </Grid>
+              <Grid item style={{ display: "flex" }}>
+                <PricingCard tier="basic" price="free"/>
+              </Grid>
+              <Grid item style={{ display: "flex" }}>
+                <PricingCard tier="premium" price="5.99 per month"/>
+              </Grid>
+            </Grid>
             
           </Box>
         </Box>
