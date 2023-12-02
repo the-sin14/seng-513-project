bffbe9fc80e60e212d6d582824a6ec323e428574
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-12-01 21:39:32 -0700

    Fixed the card issue on the landing page.

--- client/src/components/landing-page/PricingCard.tsx
+++ client/src/components/landing-page/PricingCard.tsx
@@ -1,133 +1,181 @@
-import { Link } from "react-router-dom";
-import { Box, Card, CardContent, Typography, Button } from "@mui/material"
-import { FaCheck } from "react-icons/fa6";
-import "./PricingCard.css"
+import { Link } from 'react-router-dom';
+import { Box, Typography, Button } from '@mui/material';
+import { FaCheck } from 'react-icons/fa';
+import React from 'react';
 
 type Props = {
-    tier: string
-    price: string
-}
+    tier: string;
+};
 
 export const PricingCard = (props: Props) => {
+    // Define styles using React.CSSProperties
+    const styles: Record<string, React.CSSProperties> = {
+        card: {
+            width: '100%',
+            maxWidth: '500px', 
+            margin: '10px',
+            borderRadius: '6px',
+            boxShadow: '0px 4px 12px rgba(0,0,0,0.25)',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between', 
+            height: '100%', 
+        },
+        cardContainer: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            gap: '20px', 
+           },
 
+        cardHeader: {
+            backgroundColor: '#344055',
+            color: '#F2F1EE',
+            padding: '5px 20px',
+            borderRadius: '6px 6px 0 0',
+            display: 'flex',
+            justifyContent: 'space-between', 
+            alignItems: 'center', 
+            width: '100%',
+            boxSizing: 'border-box',
+        },
+        cardContent: {
+            textAlign: 'center',
+            padding: '25px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            flex: '1', 
+        },
+        cardBody: {
+            flex: '1'
+        },
+        buttonBox: {
+            marginBottom: '20px',
+        },
+        button: {
+            fontSize: '16px',
+            width: '150px',
+            backgroundColor: '#344055',
+            color: '#F2F1EE',
+            borderRadius: '8px',
+            fontWeight: '600',
+            textTransform: 'capitalize',
+            textDecoration: 'none',
+            border: 'none',
+        },
+        feature: {
+            display: 'flex',
+            alignItems: 'center',
+            margin: '10px 0',
+        },
+        checkIcon: {
+            color: '#344055',
+            marginRight: '10px',
+        },
+        featureText: {
+            marginLeft: '10px',
+            color: "black"
+        },
+
+        bold: {
+            fontWeight: 'bold',
+        },
+        underline: {
+            textDecoration: 'underline',
+        },
+    };
+    
+    // Helper functions
     const getAccountTier = () => {
-        if (props.tier === "free") {
-            return "NO ACCOUNT"
-        } else if (props.tier === "basic") {
-            return "BASIC ACCOUNT"
-        } else if (props.tier === "premium") {
-            return "PREMIUM ACCOUNT"
+        if (props.tier === 'free') {
+            return 'NO ACCOUNT';
+        } else if (props.tier === 'basic') {
+            return 'BASIC ACCOUNT';
+        } else if (props.tier === 'premium') {
+            return 'PREMIUM ACCOUNT';
         }
-    }
+    };
 
     const getAccountPricing = () => {
-        if (props.tier === "free" || props.tier === "basic") {
-            return "FREE"
-        } else if (props.tier === "premium") {
-            return "$5/MONTH"
+        if (props.tier === 'free' || props.tier === 'basic') {
+            return 'FREE';
+        } else if (props.tier === 'premium') {
+            return '$5/MONTH';
         }
-    }
+    };
 
     const getButtonText = () => {
-        if (props.tier === "free") {
-            return "Try it out"
-        } else if (props.tier === "basic") {
-            return "Sign Up"
-        } else if (props.tier === "premium") {
-            return "Purchase"
+        if (props.tier === 'free') {
+            return 'Try It Out';
+        } else if (props.tier === 'basic') {
+            return 'Sign Up';
+        } else if (props.tier === 'premium') {
+            return 'Purchase';
         }
-    }
+    };
 
     const getNavLink = () => {
-        if (props.tier === "free") {
-            return <Button className="card-button">{getButtonText()}</Button>
-        } else if (props.tier === "basic") {
-            return (
-                <Button className="card-button">
-                    <Link to="/signup" className="link">{getButtonText()}</Link>
-                </Button>
-            )
-        } else if (props.tier === "premium") {
-            return (
-                // When users click on the "Purchase" button, it will prompt users to login
-                <Button className="card-button">
-                    <Link to="/login" className="link">{getButtonText()}</Link>
-                </Button>
-            )
-        }
-    }
+        return (
+            <Button style={styles.button}>
+                <Link to={props.tier === 'premium' ? '/login' : '/signup'} style={styles.button}>
+                    {getButtonText()}
+                </Link>
+            </Button>
+        );
+    };
 
     const getCardBody = () => {
-        if (props.tier === "free") {
+        if (props.tier === 'free') {
             return (
-                <Typography variant="body1" className="card-feature-free">
-                Want to test out Summarify? Upload your lecture slides and dive into a world of endless possibilities! When you create an account, you’ll get even more benefits and experience new heights of productivity!
+                <Typography style={{marginBottom: "50px", marginTop:"15px"}}>
+                   <span style={{color: "black"}}> 
+                    Want to test out Summarify? Upload your lecture slides and dive into a world of endless possibilities!
+                    When you create an account, you’ll get even more benefits and experience new heights of productivity!
+                   </span>
                 </Typography>
             );
-        } else if (props.tier === "basic") {
+        } else {
+            // Basic and Premium tiers share the same layout
             return (
-                <div className="card-body">
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Upload files up to <span className="bold">5,000</span> characters </Typography>
+                <div style={styles.cardBody}>
+                    <div style={styles.feature}>
+                        <FaCheck style={styles.checkIcon} />
+                        <Typography style={styles.featureText}>Upload files up to <span style={styles.bold}>5,000</span> characters</Typography>
                     </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">simple, paragraph-form</span> summaries</Typography>
+                    <div style={styles.feature}>
+                        <FaCheck style={styles.checkIcon} />
+                        <Typography style={styles.featureText}>Let AI generate <span style={styles.underline}>simple, paragraph-form</span> summaries</Typography>
                     </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">simple, point-form</span> summaries</Typography>
+                    <div style={styles.feature}>
+                        <FaCheck style={styles.checkIcon} />
+                        <Typography style={styles.featureText}>Let AI generate <span style={styles.underline}>simple, point-form</span> summaries</Typography>
                     </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">flashcards</span> for self-learning</Typography>
-                    </div>
-                </div>
-            );
-        } else if (props.tier === "premium") {
-            return (
-                <div className="card-body">
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Upload files up to <span className="bold">10,000</span> characters </Typography>
-                    </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">simple, paragraph-form</span> summaries</Typography>
-                    </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">simple, point-form</span> summaries</Typography>
-                    </div>
-                    <div className="card-feature">
-                        <FaCheck className="check-icon"/>
-                        <Typography variant="body1" className="card-feature-text">Let AI generate <span className="underline">flashcards</span> for self-learning</Typography>
+                    <div style={styles.feature}>
+                        <FaCheck style={styles.checkIcon} />
+                        <Typography style={styles.featureText}>Let AI generate <span style={styles.underline}>flashcards</span> for self-learning</Typography>
                     </div>
                 </div>
             );
         }
-    }
+    };
 
     return (
-        <Box className="card">
-            <Card>
-                <CardContent>
-                    <Box className="card-header">
-                        <Typography variant="h6" className="tier-kind">{getAccountTier()}</Typography>
-                        <Typography variant="h6" className="tier-price">{getAccountPricing()}</Typography>
-                    </Box>
 
-                    {getCardBody()}
-
-                    <Box className="button-box">
-                        {/* <Button className="card-button">{getButtonText()}</Button> */}
-                        {getNavLink()}
-                    </Box>
-                </CardContent>
-            </Card>
+        <Box style={styles.card} >
+        <Box style={styles.cardHeader}>
+            <Typography variant="h6" component="div">{getAccountTier()}</Typography>
+            <Typography variant="h6" component="div">{getAccountPricing()}</Typography>
         </Box>
-    )
+        <Box style={styles.cardContent} component="div">
+            {getCardBody()}
+            <Box style={styles.buttonBox}>
+                {getNavLink()}
+            </Box>
+        </Box>
+    </Box>
+    );
 }
 
-export default PricingCard
+export default PricingCard;
