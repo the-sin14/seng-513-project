d52e6b957dc9d9d2bcc765571d9a5defb6fff032
fhuayhualla <fabriciohuayhualla@gmail.com>
2023-12-02 20:01:05 -0700

    test commit

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,14 @@
 import {Router} from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
-
+import itemRoutes from "./itemRoutes.js";
 const appRouter = Router();
 
 appRouter.use("/user", userRoutes);
 appRouter.use("/chat", chatRoutes);
+appRouter.use("/items", itemRoutes);
+
+//const itemsRouter = require("./routes/items");
+//app.use("/api/v1/items", itemsRouter);
 
 export default appRouter;
